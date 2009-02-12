0899e6066c807c2577e1d875ec4419cfd630076f
Bob Aman <bob@sporkmonger.com>
2009-02-12 09:00:47 -0500

    Updated AST.

--- src/ast.h
+++ src/ast.h
@@ -21,5 +21,34 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **********************************************************************/
 
-typedef struct LVNode {
-} NODE;
+typedef struct _expression {
+  enum {
+    self_type=1,
+    number_type=2,
+    string_literal_type=3,
+    symbol_type=4,
+    sequence_type=5,
+    parenthetical_type=6,
+    message_type=7
+  } type;
+} expression_t;
+
+struct _stmt {
+      enum { FunctionDef_kind=1, Return_kind=2, Yield_kind=3 } kind;
+      union {
+              struct {
+                      identifier name;
+                      arguments_ty args;
+                      asdl_seq *body;
+              } FunctionDef;
+
+              struct {
+                      expr_ty value;
+              } Return;
+
+              struct {
+                      expr_ty value;
+              } Yield;
+      } v;
+      int lineno;
+    };
